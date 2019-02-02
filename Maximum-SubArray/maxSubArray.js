a320e0e3e4fb4adaba893f0dd5dedbb16c8b1a9a
Ryan <rynpowers@gmail.com>
2019-02-02 16:25:20 -0500

    added comments about complexity

--- Maximum-SubArray/maxSubArray.js
+++ Maximum-SubArray/maxSubArray.js
@@ -1,3 +1,8 @@
+// The time complexity of this problem is O(n) becuase we must traverse the
+// Array one time to get the max sub array
+
+// space is O(1) because we are not using any extra space to get our answer
+
 const maxSubArray = arr => {
   let max = arr[0];
   let sum = arr[0];
